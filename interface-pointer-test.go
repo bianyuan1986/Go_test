627db41f549a7488fa45be120f70e7138e1dd71a
baohu.rbh <baohu.rbh@alibaba-inc.com>
2020-09-21 14:53:43 +0800

    add file

---
+++ interface-pointer-test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type OutInf interface {
+	Output() bool
+}
+
+type OutSt struct {
+	name string
+}
+
+func (s *OutSt) Output() bool {
+	fmt.Printf("%s\n", s.name)
+
+	return true
+}
+
+func MyPrint(out interface{}) {
+	a := out.(OutInf)
+	a.Output()
+
+	c := out.(*OutSt)
+	c.Output()
+
+	b := out.(OutSt)
+	b.Output()
+}
+
+func main() {
+	outP := &OutSt{name: "Tiger"}
+	MyPrint(outP)
+
+	var outS OutSt
+	outS.name = "baohu"
+	MyPrint(&outS)
+
+	fmt.Println("End!")
+}
