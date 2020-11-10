f3e1be82de3bc916b11d1f57eed459fdd6160e0d
baohu.rbh <baohu.rbh@alibaba-inc.com>
2020-11-10 11:33:07 +0800

    update

--- go-lua-compare/build.sh
+++ go-lua-compare/build.sh
@@ -6,3 +6,5 @@ GOGC=off CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CC=x86_64-linux-musl-gcc CGO_LDFL
 go build -gcflags "-N -l" lua_demo.go
 #cp -f ./go-lua-compare ~/Desktop
 
+#curl http://30.27.153.142:2020/tiger_test  -H "Host: www.tiger.com" -H "Cookie: test" -H "Connection: closed" -H "UID: 202021"
+
