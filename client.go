a56a7256f336db89b3263cc067ff63bab48d1227
baohu.rbh <baohu.rbh@alibaba-inc.com>
2020-12-09 16:48:29 +0800

    update

--- client.go
+++ client.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"time"
 )
 
+/*返回值可以被命名，所以该函数返回后的返回值不为空，即err*/
+func testError() (err error) {
+	err = nil
+
+	defer func() {
+		err = errors.New("Error occured!")
+	}()
+
+	return
+}
+
 func main() {
+	err := testError()
+	if err == nil {
+		fmt.Println("Error is nil!")
+	} else {
+		fmt.Println("Error is not nil!")
+	}
 	for i := 0; i < 10; i++ {
 		fmt.Printf("Client %d\n", i)
 		net.Dial("tcp", "127.0.0.1:2020")
 	}
-	time.Sleep(time.Duration(200) * time.Second)
+	time.Sleep(time.Duration(1) * time.Second)
 }
