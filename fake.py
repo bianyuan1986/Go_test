0abfba65e48af915ee1b9d6353e663433ae57bde
baohu.rbh <baohu.rbh@alibaba-inc.com>
2020-12-11 18:32:18 +0800

    update

--- fake.py
+++ fake.py
@@ -147,21 +147,37 @@ class PacketBuilder:
     def string(self):
         return self.payload
 
-def constructPacket():
+#for rule 111090
+def constructHeaderPacket(ruleId):
     kvMap = {}
     kvMap["header.accept"] = "*/*" 
     kvMap["header.content-type"] = "text/html" 
     kvMap["header.user_agent"] = "tiger-browser" 
     kvMap["header.referer"] = "www.fake.com" 
     kvMap["header.cookie"] = "arg1=tiger&arg2=tiger2" 
-    #kvMap["first_pkt"] = "true" 
+    kvMap["first_pkt"] = "true" 
     kvMap["matched_host"] = "www.tiger.test.com" 
     kvMap["uid"] = "270828"
-    kvMap["uri"] = "/test.php?alert()"
+    if ruleId == 111044:
+        kvMap["uri"] = "courseOpenId=8w52as6seodlxrtqdeyrbw&quesType=1&dataJson=%5B%7B%22SortOrder%22%3A0%2C%22Content%22%3A%22%3Cspan style%3D%5C%22color%3A rgb(0%2C     0%2C 0)%3B font-family%3A %E5%AE%8B%E4%BD%93%3B font-size%3A 14px%3B white-space%3A pre-wrap%3B background-color%3A rgb(255%2C 255%2C 255)%3B%5C%22%3Ea%3D1 b%    3D5 c%3D1%3C%2Fspan%3E%22%2C%22IsAnswer%22%3Afalse%7D%2C%7B%22SortOrder%22%3A1%2C%22Content%22%3A%22%3Cdiv%3E%3Cspan style%3D%5C%22background-color%3A rgb(255    %2C 255%2C 255)%3B color%3A rgb(0%2C 0%2C 0)%3B font-family%3A %E5%AE%8B%E4%BD%93%3B font-size%3A 14px%3B white-space%3A pre-wrap%3B%5C%22%3Ea%3D5 b%3D3 c%3D1    %3C%2Fspan%3E%3Cbr%3E%3C%2Fdiv%3E%22%2C%22IsAnswer%22%3Afalse%7D%2C%7B%22SortOrder%22%3A2%2C%22Content%22%3A%22%3Cspan style%3D%5C%22color%3A rgb(0%2C 0%2C 0)    %3B font-family%3A %E5%AE%8B%E4%BD%93%3B font-size%3A 14px%3B white-space%3A pre-wrap%3B background-color%3A rgb(255%2C 255%2C 255)%3B%5C%22%3Ea%3D1 b%3D3 c%3    D1%3C%2Fspan%3E%22%2C%22IsAnswer%22%3Atrue%7D%2C%7B%22SortOrder%22%3A3%2C%22Content%22%3A%22%3Cdiv%3E%3Cspan style%3D%5C%22background-color%3A rgb(255%2C 255%    2C 255)%3B color%3A rgb(0%2C 0%2C 0)%3B font-family%3A %E5%AE%8B%E4%BD%93%3B font-size%3A 14px%3B white-space%3A pre-wrap%3B%5C%22%3Ea%3D5 b%3D3 c%3D5%3C%2Fsp    an%3E%3Cbr%3E%3C%2Fdiv%3E%22%2C%22IsAnswer%22%3Afalse%7D%5D&questionId=&pageSize=10&page=1&DefficultyLevel=1&knowledgeTitle=%E9%80%89%E6%8B%A9%E7%BB%93%E6%9E%    84%E8%87%AA%E6%B5%8B&knowledgeIds=c7pqaiqsjitkyiy7p2edmw&Title=%3Cp%3E%E5%81%87%E5%AE%9A%E6%89%80%E6%9C%89%E5%8F%98%E9%87%8F%E5%9D%87%E5%B7%B2%E6%AD%A3%E7%A1%    AE%E8%AF%B4%E6%98%8E%EF%BC%8C%E4%BB%A5%E4%B8%8B%E7%A8%8B%E5%BA%8F%E6%AE%B5%E8%BF%90%E8%A1%8C%E5%90%8E%E7%9A%84%E8%BE%93%E5%87%BA%E7%BB%93%E6%9E%9C%E6%98%AF%26    nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B %E3%80%82%3C%2Fp%3E%3Cp%3Eint a%3D1%2Cb%3D5%2Cc%3D3%3B%3C%2Fp%3E%3Cp%3Eif(a%26gt%3Bb)%3C%2Fp%3E%3Cp%3Ea%3Db%3B    %3C%2Fp%3E%3Cp%3Eb%3Dc%3B%3C%2Fp%3E%3Cp%3Ec%3Da%3B%3C%2Fp%3E%3Cp%3Eprintf(%26quot%3Ba%3D%25d b%3D%25d c%3D%25d%5Cn%26quot%3B%2Ca%2Cb%2Cc)%3B%3C%2Fp%3E%3Cp%3E%    3Cbr%2F%3E%3C%2Fp%3E%3Cp%3E%3Cbr%2F%3E%3C%2Fp%3E&answer=2&ResultAnalysis="
+    else:
+        kvMap["uri"] = "/test.php?alertd()"
+    if ruleId == 111090:
+        kvMap["body"] = "search=select trunc( categoryid/pkg_roles.Fgetmancat(2)) as CATEGORYID, sum(nvl(a.salevalue,0)) as salevalue, sum(nvl(a.netsalevalue,0)) as netsalevalue, sum(nvl(a.salevalueratio,0)) as salevalueratio, sum(nvl(a.costvalue,0)) as costvalue, sum(nvl(a.netcostvalue,0)) as netcostvalue, sum(nvl(a.grossprofit,0)) as grossprofit, sum(nvl(a.grossprofit,0))/f0tonull( sum(nvl(a.salevalue,0)))*100 as grossmargin, sum(nvl(a.grossprofitratio,0)) as grossprofitratio,salechannel from TMP_RPT_M85151006 a where 1=1 group by salechannel, trunc( categoryid/pkg_roles.Fgetmancat(2)) order by trunc( categoryid/pkg_roles.Fgetmancat(2))"
+    elif ruleId == 111044:
+        kvMap["body"] = "courseOpenId=8w52as6seodlxrtqdeyrbw&quesType=1&dataJson=%5B%7B%22SortOrder%22%3A0%2C%22Content%22%3A%22%3Cspan style%3D%5C%22color%3A rgb(0%2C     0%2C 0)%3B font-family%3A %E5%AE%8B%E4%BD%93%3B font-size%3A 14px%3B white-space%3A pre-wrap%3B background-color%3A rgb(255%2C 255%2C 255)%3B%5C%22%3Ea%3D1 b%    3D5 c%3D1%3C%2Fspan%3E%22%2C%22IsAnswer%22%3Afalse%7D%2C%7B%22SortOrder%22%3A1%2C%22Content%22%3A%22%3Cdiv%3E%3Cspan style%3D%5C%22background-color%3A rgb(255    %2C 255%2C 255)%3B color%3A rgb(0%2C 0%2C 0)%3B font-family%3A %E5%AE%8B%E4%BD%93%3B font-size%3A 14px%3B white-space%3A pre-wrap%3B%5C%22%3Ea%3D5 b%3D3 c%3D1    %3C%2Fspan%3E%3Cbr%3E%3C%2Fdiv%3E%22%2C%22IsAnswer%22%3Afalse%7D%2C%7B%22SortOrder%22%3A2%2C%22Content%22%3A%22%3Cspan style%3D%5C%22color%3A rgb(0%2C 0%2C 0)    %3B font-family%3A %E5%AE%8B%E4%BD%93%3B font-size%3A 14px%3B white-space%3A pre-wrap%3B background-color%3A rgb(255%2C 255%2C 255)%3B%5C%22%3Ea%3D1 b%3D3 c%3    D1%3C%2Fspan%3E%22%2C%22IsAnswer%22%3Atrue%7D%2C%7B%22SortOrder%22%3A3%2C%22Content%22%3A%22%3Cdiv%3E%3Cspan style%3D%5C%22background-color%3A rgb(255%2C 255%    2C 255)%3B color%3A rgb(0%2C 0%2C 0)%3B font-family%3A %E5%AE%8B%E4%BD%93%3B font-size%3A 14px%3B white-space%3A pre-wrap%3B%5C%22%3Ea%3D5 b%3D3 c%3D5%3C%2Fsp    an%3E%3Cbr%3E%3C%2Fdiv%3E%22%2C%22IsAnswer%22%3Afalse%7D%5D&questionId=&pageSize=10&page=1&DefficultyLevel=1&knowledgeTitle=%E9%80%89%E6%8B%A9%E7%BB%93%E6%9E%    84%E8%87%AA%E6%B5%8B&knowledgeIds=c7pqaiqsjitkyiy7p2edmw&Title=%3Cp%3E%E5%81%87%E5%AE%9A%E6%89%80%E6%9C%89%E5%8F%98%E9%87%8F%E5%9D%87%E5%B7%B2%E6%AD%A3%E7%A1%    AE%E8%AF%B4%E6%98%8E%EF%BC%8C%E4%BB%A5%E4%B8%8B%E7%A8%8B%E5%BA%8F%E6%AE%B5%E8%BF%90%E8%A1%8C%E5%90%8E%E7%9A%84%E8%BE%93%E5%87%BA%E7%BB%93%E6%9E%9C%E6%98%AF%26    nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B%26nbsp%3B %E3%80%82%3C%2Fp%3E%3Cp%3Eint a%3D1%2Cb%3D5%2Cc%3D3%3B%3C%2Fp%3E%3Cp%3Eif(a%26gt%3Bb)%3C%2Fp%3E%3Cp%3Ea%3Db%3B    %3C%2Fp%3E%3Cp%3Eb%3Dc%3B%3C%2Fp%3E%3Cp%3Ec%3Da%3B%3C%2Fp%3E%3Cp%3Eprintf(%26quot%3Ba%3D%25d b%3D%25d c%3D%25d%5Cn%26quot%3B%2Ca%2Cb%2Cc)%3B%3C%2Fp%3E%3Cp%3E%    3Cbr%2F%3E%3C%2Fp%3E%3Cp%3E%3Cbr%2F%3E%3C%2Fp%3E&answer=2&ResultAnalysis="
+    else:
+        kvMap["body"] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>"
+        #kvMap["body"] = "<html><head><title>tiger</title></head><body></body></html>"
+
+    return constructPacket(kvMap)
+
+def constructPacket(kvMap):
     bodyLen = 0
     for (k,v) in kvMap.items():
-        bodyLen += 8
+        bodyLen += 4
         bodyLen += len(k)
+        bodyLen += 4
         bodyLen += len(v)
     print "bodyLen:%d" % bodyLen
 
@@ -173,6 +189,7 @@ def constructPacket():
     builder.setRequestIdx(2)
     builder.setPadding(4)
     print "headerLen:%d" % len(builder.string())
+
     for (k,v) in kvMap.items():
         builder.setKeyValue(k, v)
     pkt = builder.string()
@@ -215,11 +232,13 @@ def DumpPacket(data, dLen):
 unix_sock_path = "/home/admin/warden/run/ngwaf.sock"
 
 if __name__ == "__main__":
-    pkt = constructPacket()
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    try:
-        s.connect(unix_sock_path)
-        s.sendall(pkt)
-        print "Warden response:\n%s" % s.recv(1024)
-    finally:
-        s.close()
+    ids = [0, 111044, 111090]
+    for i in range(0, len(ids)):
+        pkt = constructHeaderPacket(ids[i])
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            s.connect(unix_sock_path)
+            s.sendall(pkt)
+            print "Warden response:\n%s" % s.recv(1024)
+        finally:
+            s.close()
