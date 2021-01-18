30209aef736f66b026664db9d1c1599cc8e1b157
baohu.rbh <baohu.rbh@alibaba-inc.com>
2021-01-18 18:35:55 +0800

    update

--- reflect/goReflect.go
+++ reflect/goReflect.go
@@ -15,6 +15,8 @@ type rule struct {
 	Name   string
 	SRules subRule
 	MRules []subRule
+	PRules []*subRule
+	TRules map[string]*subRule
 }
 
 func testReflect() {
@@ -23,6 +25,9 @@ func testReflect() {
 	r.MRules = append(r.MRules, subRule{Sid: 4, Sname: "eee"})
 	r.SRules.Sid = 3
 	r.SRules.Sname = "ddd"
+	r.PRules = append(r.PRules, &subRule{Sid: 5, Sname: "eee"})
+	r.TRules = make(map[string]*subRule)
+	r.TRules["2021"] = &subRule{Sid: 6, Sname: "eee"}
 	parseStructType(r, 0)
 }
 
@@ -64,9 +69,12 @@ func parseStructType(data interface{}, level int) interface{} {
 		subType := t.Field(i).Type
 		printPrefix(level + 1)
 		fmt.Printf("SubType:%15s |", subType)
+
+	REPEAT:
 		kind := subValue.Kind()
 		/*printPrefix(level)
-		fmt.Printf("Kind: [%s]\n", kind.String())*/
+		fmt.Printf("Kind: [%15s]\n", kind.String())*/
+
 		switch kind {
 		case reflect.Int:
 			fallthrough
@@ -83,8 +91,51 @@ func parseStructType(data interface{}, level int) interface{} {
 			if elementType.Kind() == reflect.Struct {
 				element := reflect.New(elementType).Interface()
 				parseStructType(element, level+1)
+			} else if elementType.Kind() == reflect.Ptr {
+				subValue = subValue.Index(0).Elem()
+				subType = subValue.Type()
+				goto REPEAT
 			}
+		case reflect.Ptr:
+			subValue = subValue.Elem()
+			subType = subValue.Type()
+			fmt.Println("Type:", subType)
+			goto REPEAT
+		case reflect.Array:
 		case reflect.Map:
+			/*keyType := subValue.Type().Key()
+			fmt.Println("Key Type:", subType)*/
+			iter := subValue.MapRange()
+			notExhausted := iter.Next()
+			if notExhausted == false {
+				continue
+			}
+			k := iter.Key()
+			v := iter.Value()
+			keyType := k.Type()
+			if keyType.Kind() == reflect.Struct {
+				element := reflect.New(keyType).Interface()
+				parseStructType(element, level+1)
+			} else if keyType.Kind() == reflect.Ptr {
+				subValue = k.Elem()
+				subType = subValue.Type()
+				goto REPEAT
+			} else {
+				fmt.Println("Key Type:", keyType)
+			}
+
+			valueType := v.Type()
+			if valueType.Kind() == reflect.Struct {
+				element := reflect.New(keyType).Interface()
+				parseStructType(element, level+1)
+			} else if valueType.Kind() == reflect.Ptr {
+				subValue = v.Elem()
+				subType = subValue.Type()
+				goto REPEAT
+			} else {
+				fmt.Println("Value Type:", valueType)
+			}
+
 		default:
 			fmt.Println("Unsupported value!")
 		}
@@ -111,8 +162,9 @@ func main() {
 }
 
 /*
+bianyuan@B-P494MD6P-2239 reflect % ./goReflect
 Type of data: main.rule
-Struct [rule] contain [4] fields
+Struct [rule] contain [6] fields
     ------------------------------
     [00]: [Id]
     SubType:            int |Value: 2021
@@ -140,6 +192,30 @@ Struct [rule] contain [4] fields
         SubType:         string |Value:
         ------------------------------
         New Value: {2022 sql}
+    [04]: [PRules]
+    SubType:[]*main.subRule |Type: *main.subRule
+Value: {5 eee}
+    Type of data:main.subRule
+    Struct [subRule] contain [2] fields
+        ------------------------------
+        [00]: [Sid]
+        SubType:            int |Value: 0
+        [01]: [Sname]
+        SubType:         string |Value:
+        ------------------------------
+        New Value: {2022 sql}
+    [05]: [TRules]
+    SubType:map[string]*main.subRule |Key Type: string
+Value: {6 eee}
+    Type of data:main.subRule
+    Struct [subRule] contain [2] fields
+        ------------------------------
+        [00]: [Sid]
+        SubType:            int |Value: 0
+        [01]: [Sname]
+        SubType:         string |Value:
+        ------------------------------
+        New Value: {2022 sql}
     ------------------------------
-    New Value: {2022 sql {0 } []}
+    New Value: {2022 sql {0 } [] [] map[]}
 */
